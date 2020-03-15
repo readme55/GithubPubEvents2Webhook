d44225ab9a52f9e115153e23a4a187f9cb5d6c15
readme <readme55@protonmail.com>
2020-03-15 15:23:17 +0100

    update

--- gh-dashevo2discord.py
+++ gh-dashevo2discord.py
@@ -4,10 +4,12 @@ from jsondiff import diff
 import sys
 import time
 
+# src
 eventUrl = "https://api.github.com/users/dashevo/events/public"
+# dest
 webhook_url = "xxx"
 data_cur = ''
-firstRun = False    # set True to skip existing data
+firstRun = True    # set True to skip existing data
 
 while True:
     response = requests.get(eventUrl)
@@ -16,45 +18,54 @@ while True:
     data_cur = json.loads(response.content)
     data_diff = diff(data_old, data_cur)
 
-    # if list is empty    
-    # if not data_diff:
+    # if list is empty
+    if not data_diff:
+        # print "sleep"
+        time.sleep(61)  # only 60/hour api calls on github-events allowed
+        continue
 
-    #if list is not empty    
-    if data_diff:
-        for item in data_diff[::-1]:    # start with last element
-            msg = ''
-            msg += ('__**Type:**__            __' + item['type'] + '__' + '\\n')
-            msg += ('**Actor:**           ' + item['actor']['login'] + '\\n').encode('ascii', 'ignore').decode('ascii')
-            msg += ('**Repo:**            ' + item['repo']['name'] + '\\n')
+    for item in data_diff[::-1]:    # start with last element
+        msg = ''
+        msg += ('__**Type:**__            __' +
+                item['type'] + '__' + '\\n')
+        msg += ('**Actor:**           ' +
+                item['actor']['login'] + '\\n').encode('ascii', 'ignore').decode('ascii')
+        msg += ('**Repo:**            ' + item['repo']['name'] + '\\n')
 
-            if 'ref' in item['payload']:
-                msg += ('**Ref:**                ' + item['payload']['ref'] + '\\n')
-            if 'commits' in item['payload']:
-                # print item['payload']['commits'][0]['author']['name']
-                msg += ('**Message:**     ' + item['payload']['commits'][0]['message'] + '\\n').encode('ascii', 'ignore').decode('ascii')
+        if 'ref' in item['payload']:
+            msg += ('**Ref:**                ' +
+                    item['payload']['ref'] + '\\n')
 
-            if 'action' in item['payload']:
-                msg += ('**Action:**         ' + item['payload']['action'] + '\\n')
-            if 'comment' in item['payload']:
-                msg += ('**Body:**            ' + item['payload']['comment']['body'] + '\\n').encode('ascii', 'ignore').decode('ascii')
-                # if 'original_commit_id' in item['payload']['comment']:
-                    # Filter for PushEvents, else wrong data when eg PullRequestReviewCommentEvent    
-                    # msg += '**Link:**        ' + 'https://github.com/' + item['repo']['name'] + '/commit/' + item['payload']['comment']['original_commit_id'] + '\\n'
+        # Filter for PushEvents
+        if 'head' in item['payload']:
+            msg += '**Link:**        ' + 'https://github.com/' + \
+                item['repo']['name'] + '/commit/' + \
+                item['payload']['head'] + '\\n'
 
-            msg += '**Created at:**  ' + item['created_at'] + '\\n'
-            # msg += '==================================' + '\\n'
-            print msg
-            
-            # if firstRun == False:
-            #     myjson = '{"username": "GitHub-dashevo", "content": "' + msg + '"}'
-            #     response = requests.post(
-            #         webhook_url, myjson,
-            #         headers={'Content-Type': 'application/json'
-            #         }
-            #     )
-                # print response
-                # exit()
+        if 'commits' in item['payload']:
+            # msg += (item['payload']['commits'][0]['author']['name']).decode('ascii')
+            msg += ('**Message:**     ' + item['payload']['commits'][0]
+                    ['message'] + '\\n').encode('ascii', 'ignore').decode('ascii')
+
+        if 'action' in item['payload']:
+            msg += ('**Action:**         ' +
+                    item['payload']['action'] + '\\n')
+        if 'comment' in item['payload']:
+            msg += ('**Body:**            ' + item['payload']['comment']
+                    ['body'] + '\\n').encode('ascii', 'ignore').decode('ascii')
+            # if 'original_commit_id' in item['payload']['comment']:
+
+        msg += '**Created at:**  ' + item['created_at'] + '\\n'
+        # msg += '==================================' + '\\n'
+        # print msg
+
+        if firstRun == False:
+            myjson = '{"username": "GitHub-dashevo", "content": "' + msg + '"}'
+            response = requests.post(
+                webhook_url, myjson,
+                headers={'Content-Type': 'application/json'}
+            )
+        # print response
+        # exit()
 
     firstRun = False
-    print "sleep"
-    time.sleep(5)
