b8945496fcbd8becdb8756c88a9ca56eef4e677d
readme <readme55@protonmail.com>
2020-03-16 12:58:56 +0100

    major update: add more info on PushEvent, add more links, catch TypeError, load webhook url from file

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # GithubPubEvents2Webhook
 
 ## TODO:
-- check if more Links possible
-- check where links with the format https://github.com/dashevo/dash-network-deploy/pull/127 come from
-- at last overwrite embed somehow (which currently show with PushEvents) (https://birdie0.github.io/discord-webhooks-guide/discord_webhook.html)
+- edit/exchange python formatter so longer lines are shown (yapf)
+- check if any informations/Links are missing
+- check if script runs stable
+- (optional) check https://developer.github.com/v3/#conditional-requests and integrate Conditional requests with `If-Modified-Since`
+- (optional) when all infos are caught and integrated + running stable: create pretty embed (https://birdie0.github.io/discord-webhooks-guide/discord_webhook.html)
--- gh-dashevo2discord.py
+++ gh-dashevo2discord.py
@@ -7,67 +7,126 @@ import datetime
 
 # src
 eventUrl = "https://api.github.com/users/dashevo/events/public"
-# dest
-webhook_url = "xxx"
+# dest  (non-public so i put it in file and .gitignore)
+f=open("webhook.txt", "r")
+if f.mode == 'r':
+    webhook_url = f.read()
+
 data_cur = ''
-firstRun = True    # set True to skip existing data
+firstRun = True  # set True to skip existing data
 
 while True:
-    response = requests.get(eventUrl)
+    try:
+        response = requests.get(eventUrl)
 
-    data_old = data_cur
-    data_cur = json.loads(response.content)
-    data_diff = diff(data_old, data_cur)
+        data_old = data_cur
+        data_cur = json.loads(response.content)
+        data_diff = diff(data_old, data_cur)
 
-    # if list is empty
-    if not data_diff:
-        # print "sleep"
-        time.sleep(61)  # only 60/hour api calls on github-events allowed
-        continue
+        # if list is empty
+        if not data_diff:
+            time.sleep(61)  # only 60/hour api calls on github-events allowed
+            continue
 
-    for item in data_diff[::-1]:    # start with last element
-        msg = ''
-        msg += ('__**Type:**__            __' +
-                item['type'] + '__' + '\\n')
-        msg += ('**Actor:**           ' +
-                item['actor']['login'] + '\\n').encode('ascii', 'ignore').decode('ascii')
-        msg += ('**Repo:**            ' + item['repo']['name'] + '\\n')
+        for item in data_diff[::-1]:  # start with last element
+            msg = ''
+            msg += ('__**Type:**__            __' + item['type'] + '__' +
+                    '\\n')
 
-        if 'ref' in item['payload']:
-            msg += ('**Ref:**                ' +
-                    item['payload']['ref'] + '\\n')
+            msg += ('**Repo:**            ' + item['repo']['name'] + '\\n')
 
-        # Filter for PushEvents
-        if 'head' in item['payload']:
-            msg += '**Link:**        ' + 'https://github.com/' + \
-                item['repo']['name'] + '/commit/' + \
-                item['payload']['head'] + '\\n'
+            if 'ref' in item['payload']:
+                msg += ('**Ref:**                ' + item['payload']['ref'] +
+                        '\\n')
 
-        if 'commits' in item['payload']:
-            # msg += (item['payload']['commits'][0]['author']['name']).decode('ascii')
-            msg += ('**Message:**     ' + item['payload']['commits'][0]
-                    ['message'] + '\\n').encode('ascii', 'ignore').decode('ascii')
+            msg += ('**Actor:**           ' + item['actor']['login'] +
+                    '\\n').encode('ascii', 'ignore').decode('ascii')
 
-        if 'action' in item['payload']:
-            msg += ('**Action:**         ' +
-                    item['payload']['action'] + '\\n')
-        if 'comment' in item['payload']:
-            msg += ('**Body:**            ' + item['payload']['comment']
-                    ['body'] + '\\n').encode('ascii', 'ignore').decode('ascii')
-            # if 'original_commit_id' in item['payload']['comment']:
+            ## PushEvent (recognize from 'payload' objects)
+            # if 'head' in item['payload']:
+            #     msg += '**Link:**        ' + 'https://github.com/' + \
+            #         item['repo']['name'] + '/commit/' + \
+            #         item['payload']['head'] + '\\n'
 
-        msg += '**Created at:**  ' + item['created_at'] + '\\n'
-        # msg += '==================================' + '\\n'
-        # print msg
+            # (Multiple) Commit messages (Author Name + Message + HTML Link)
+            if 'commits' in item['payload']:
+                msg += '**Commits:**' + '\\n'
+                for commit in item['payload']['commits']:
+                    msg += ('**__Author__:**     ' +
+                            commit['author']['name']).decode('ascii')
+                    msg += ('**Message:**     ' + commit['message'] +
+                            '\\n').encode('ascii', 'ignore').decode('ascii')
+                    msg += ('**Link:**     ' + 'https://github.com/' +
+                            item['repo']['name'] + '/commit/' + commit['sha'] +
+                            '\\n')
 
-        if firstRun == False:
-            myjson = '{"username": "GitHub-dashevo", "content": "' + msg + '"}'
-            response = requests.post(
-                webhook_url, myjson,
-                headers={'Content-Type': 'application/json'}
-            )
-        print datetime.datetime.now()
-        print response
-        # exit()
+                    # msg += ('**Author:**     ' + item['payload']['commits'][0]['author']['name']
+                    #         ).decode('ascii')
+                    # msg += ('**Message:**     ' +
+                    #         item['payload']['commits'][0]['message'] + '\\n').encode(
+                    #             'ascii', 'ignore').decode('ascii')
 
-    firstRun = False
+            ## PullRequestEvent, ReleaseEvent, IssueCommentEvent
+            if 'action' in item['payload']:
+                msg += ('**Action:**         ' + item['payload']['action'] +
+                        '\\n')
+
+            if 'pull_request' in item['payload']:
+                msg += ('**Link:**       ' +
+                        item['payload']['pull_request']['html_url'] + '\\n')
+                msg += ('**Title:**       ' +
+                        item['payload']['pull_request']['title'] +
+                        '\\n').encode('ascii', 'ignore').decode('ascii')
+                msg += ('**Body:**       ' +
+                        item['payload']['pull_request']['body'] +
+                        '\\n').encode('ascii', 'ignore').decode('ascii')
+
+            if 'release' in item['payload']:
+                msg += ('**Link:**       ' +
+                        item['payload']['release']['html_url'] + '\\n')
+                msg += ('**Name:**       ' +
+                        item['payload']['release']['name'] + '\\n').encode(
+                            'ascii', 'ignore').decode('ascii')
+                msg += ('**Body:**       ' +
+                        item['payload']['release']['body'] + '\\n').encode(
+                            'ascii', 'ignore').decode('ascii')
+
+            if 'issue' in item['payload']:
+                msg += ('**Link:**       ' +
+                        item['payload']['issue']['html_url'] + '\\n')
+                msg += ('**Title:**       ' +
+                        item['payload']['issue']['title'] + '\\n').encode(
+                            'ascii', 'ignore').decode('ascii')
+                msg += ('**Body:**       ' + item['payload']['issue']['body'] +
+                        '\\n').encode('ascii', 'ignore').decode('ascii')
+
+            ## PullRequestReviewCommentEvent
+            if 'comment' in item['payload']:
+                msg += ('**Body:**            ' +
+                        item['payload']['comment']['body'] + '\\n').encode(
+                            'ascii', 'ignore').decode('ascii')
+                msg += ('**Link:**            ' +
+                        item['payload']['comment']['html_url'] + '\\n')
+
+            msg += '**Created at:**  ' + item['created_at'] + '\\n'
+
+            ## Debug local
+            # print msg
+
+            if firstRun == False:
+                myjson = '{"username": "GitHub-dashevo", "content": "' + msg + '"}'
+                response = requests.post(
+                    webhook_url,
+                    myjson,
+                    headers={'Content-Type': 'application/json'})
+            print datetime.datetime.now()
+            print response
+
+        firstRun = False
+
+    except TypeError:
+        print("TypeError: prob API limit exceed! Sleeping 61 min")
+        time.sleep(3660)
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt: Goodbye")
+        exit()
